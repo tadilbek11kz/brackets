88583b31982cdc2cda4ae6050c28afb8b20f308f
Adilbek11kz <tadilbek11kz@gmail.com>
2020-10-14 17:52:52 +0600

    init: create project brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const brackets = Object.fromEntries(bracketsConfig);
+
+  for (const element of str.split('')) {
+    if (stack[0] === element) {
+      stack.shift();
+    } else if (brackets[element]) {
+      stack.unshift(brackets[element]);
+    } else {
+      return false;
+    }
+  }
+  return stack.length === 0;
 }
